518ef01a193abe8bf396e267b693106df5cca421
bdimpe@gmail <bdimpe@gmail>
2016-02-18 17:27:39 -0500

    back end appears to be working

--- OData/App_Start/WebApiConfig.cs
+++ OData/App_Start/WebApiConfig.cs
@@ -5,31 +5,32 @@ using System.Web.Http;
 
 namespace OData
 {
-	using System.Web.Http.OData.Builder;
-	using System.Web.Routing;
+    using System.Web.Http.OData.Builder;
+    using System.Web.Routing;
 
-	using OdataGenerator;
+    using OdataGenerator;
 
-	public static class WebApiConfig
-	{
-		public const string DefaultODataRoute = "DefaultOData";
+    public static class WebApiConfig
+    {
+        public const string DefaultODataRoute = "DefaultOData";
 
-		public static void Register(HttpConfiguration config)
-		{
+        public static void Register(HttpConfiguration config)
+        {
+            //config.Formatters.XmlFormatter.SupportedMediaTypes.Remove()
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            var modelBuilder = new ODataConventionModelBuilder();
+            ODataConventionModelBuilderEnabler.WireModels(modelBuilder);
 
-			var modelBuilder = new ODataConventionModelBuilder();
-			ODataConventionModelBuilderEnabler.WireModels(modelBuilder);
+            var model = modelBuilder.GetEdmModel();
+            config.Routes.MapODataRoute("OData", "OData", model);
+            var odataRoute = RouteTable.Routes["OData"] as Route;
 
-			var model = modelBuilder.GetEdmModel();
-			config.Routes.MapODataRoute("OData", "OData", model);
-			var odataRoute = RouteTable.Routes["OData"] as Route;
-            
-			config.Routes.MapHttpRoute(DefaultODataRoute, "OData/{controller}/{id}", new { id = RouteParameter.Optional });
-			config.Routes.MapHttpRoute(
-					name: "DefaultApi",
-					routeTemplate: "api/{controller}/{id}",
-					defaults: new { id = RouteParameter.Optional }
-			);
-		}
-	}
+            config.Routes.MapHttpRoute(DefaultODataRoute, "OData/{controller}/{id}", new { id = RouteParameter.Optional });
+            config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
 }
--- Webby/VirtualWcfFactory.cs
+++ Webby/VirtualWcfFactory.cs
@@ -1,153 +1,168 @@
 ﻿namespace Webby
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Data.Services;
-	using System.Linq;
-	using System.ServiceModel;
-	using System.ServiceModel.Activation;
-	using System.ServiceModel.Description;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Services;
+    using System.Linq;
+    using System.ServiceModel;
+    using System.ServiceModel.Activation;
+    using System.ServiceModel.Description;
 
-	using Contracts;
+    using Contracts;
 
-	public class VirtualWcfFactory : ServiceHostFactory
-	{
-		private static readonly IDictionary<string, Type> TypeMap = new Dictionary<string, Type>(StringComparer.InvariantCultureIgnoreCase);
+    public class VirtualWcfFactory : ServiceHostFactory
+    {
+        static readonly IDictionary<string, Type> TypeMap = new Dictionary<string, Type>(StringComparer.InvariantCultureIgnoreCase);
 
-		static bool initialized = false;
-		private static readonly object Lock = new object();
+        static bool initialized = false;
+        static readonly object Lock = new object();
 
-		private static Type GetType(string typeString)
-		{
-			Type serviceType;
+        class GetTypeResult
+        {
+            public Type Type { get; set; }
+            public Exception Exception { get; set; }
+            public bool IsSuccess => Exception == null && Type != null;
+        }
 
-			if (TypeMap.TryGetValue(typeString, out serviceType))
-			{
-				return serviceType;
-			}
-			
-			lock (Lock)
-			{
-				if (!initialized)
-				{
-					initialized = true;
-					
-				}
-				if (!TypeMap.ContainsKey(typeString))
-				{
-					
-					try
-					{
-						serviceType = Type.GetType(typeString, true, true);
-					}
-					catch (System.IO.FileNotFoundException fileNotFoundException)
-					{
-						System.Diagnostics.Debugger.Launch();
-						throw;
+        static GetTypeResult TryGetType(string typeString)
+        {
+            try
+            {
+                var type = Type.GetType(typeString, true, true);
+                return new GetTypeResult { Type = type };
+            }
+            catch (System.IO.FileNotFoundException fEx)
+            {
+                return new GetTypeResult { Exception = fEx };
+            }
+            catch (TypeLoadException tEx)
+            {
+                return new GetTypeResult { Exception = tEx };
+            }
+        }
 
-					}
-					catch (TypeLoadException typeLoadException)
-					{
-						System.Diagnostics.Debugger.Launch();
-						throw;
-					}
-					
+        static Type GetType(string typeString)
+        {
+            Type serviceType;
 
-					TypeMap.Add(typeString, serviceType);
-				}
-				else
-				{
-					serviceType = TypeMap[typeString];
-				}
+            if (TypeMap.TryGetValue(typeString, out serviceType))
+                return serviceType;
 
-				return serviceType;
-			}
-		}
+            lock (Lock)
+            {
+                if (!initialized)
+                {
+                    initialized = true;
+                }
 
-		private static void ConfigureService(Type iface, ServiceHost host)
-		{
-			var proxyConfig = ProxyConfiguration.Create(new Config(), iface.Name.Substring(1));
+                if (!TypeMap.ContainsKey(typeString))
+                {
+                    var result = TryGetType(typeString);
+                    if (!result.IsSuccess)
+                        result = TryGetType(typeString + "Service");
+                    if (!result.IsSuccess)
+                        result = TryGetType(typeString.Replace(",", "Service,"));
+                    if (!result.IsSuccess)
+                    {
+                        System.Diagnostics.Debugger.Launch();
+                        throw new System.TypeLoadException("Failed to load " + typeString, result.Exception);
+                    }
 
-			//host.Authorization.ServiceAuthorizationManager = new CompanyNameServiceAuthorizationManager();
+                    TypeMap.Add(typeString, result.Type);
+                }
+                else
+                {
+                    serviceType = TypeMap[typeString];
+                }
 
-			var ep = host.AddServiceEndpoint(iface, new BindingFactory().CreateBinding(proxyConfig), string.Empty);
+                return serviceType;
+            }
+        }
 
-			//ep.Behaviors.Add(new ContextPropagator());
+        static void ConfigureService(Type iface, ServiceHost host)
+        {
+            var proxyConfig = ProxyConfiguration.Create(new Config(), iface.Name.Substring(1));
 
-			if (proxyConfig.BindingType == typeof(WebHttpBinding))
-			{
-				ep.Behaviors.Add(new WebHttpBehavior());
-			}
+            //host.Authorization.ServiceAuthorizationManager = new CompanyNameServiceAuthorizationManager();
 
-			foreach (var op in ep.Contract.Operations)
-			{
-				var dataContract = op.Behaviors.Find<DataContractSerializerOperationBehavior>();
+            var ep = host.AddServiceEndpoint(iface, new BindingFactory().CreateBinding(proxyConfig), string.Empty);
 
-				if (dataContract == null)
-				{
-					dataContract = new DataContractSerializerOperationBehavior(op) { MaxItemsInObjectGraph = proxyConfig.MaxItemsInObjectGraph };
-					op.Behaviors.Add(dataContract);
-				}
-				else
-				{
-					dataContract.MaxItemsInObjectGraph = proxyConfig.MaxItemsInObjectGraph;
-				}
-			}
+            //ep.Behaviors.Add(new ContextPropagator());
 
-			var serviceDebugBehavior = host.Description.Behaviors.Find<ServiceDebugBehavior>();
+            if (proxyConfig.BindingType == typeof(WebHttpBinding))
+            {
+                ep.Behaviors.Add(new WebHttpBehavior());
+            }
 
-			if (serviceDebugBehavior == null)
-			{
-				serviceDebugBehavior = new ServiceDebugBehavior { IncludeExceptionDetailInFaults = proxyConfig.IncludeExceptionDetailInFaults };
-				host.Description.Behaviors.Add(serviceDebugBehavior);
-			}
-			else
-			{
-				serviceDebugBehavior.IncludeExceptionDetailInFaults = proxyConfig.IncludeExceptionDetailInFaults;
-			}
+            foreach (var op in ep.Contract.Operations)
+            {
+                var dataContract = op.Behaviors.Find<DataContractSerializerOperationBehavior>();
 
-			var metadataBehavior = host.Description.Behaviors.Find<ServiceMetadataBehavior>();
+                if (dataContract == null)
+                {
+                    dataContract = new DataContractSerializerOperationBehavior(op) { MaxItemsInObjectGraph = proxyConfig.MaxItemsInObjectGraph };
+                    op.Behaviors.Add(dataContract);
+                }
+                else
+                {
+                    dataContract.MaxItemsInObjectGraph = proxyConfig.MaxItemsInObjectGraph;
+                }
+            }
 
-			if (metadataBehavior == null)
-			{
-				metadataBehavior = new ServiceMetadataBehavior { HttpGetEnabled = true };
-				host.Description.Behaviors.Add(metadataBehavior);
-			}
-			else
-			{
-				metadataBehavior.HttpGetEnabled = true;
-			}
-		}
+            var serviceDebugBehavior = host.Description.Behaviors.Find<ServiceDebugBehavior>();
 
-		public override ServiceHostBase CreateServiceHost(string constructorString, Uri[] baseAddresses)
-		{
-			var serviceType = GetType(constructorString);
+            if (serviceDebugBehavior == null)
+            {
+                serviceDebugBehavior = new ServiceDebugBehavior { IncludeExceptionDetailInFaults = proxyConfig.IncludeExceptionDetailInFaults };
+                host.Description.Behaviors.Add(serviceDebugBehavior);
+            }
+            else
+            {
+                serviceDebugBehavior.IncludeExceptionDetailInFaults = proxyConfig.IncludeExceptionDetailInFaults;
+            }
 
-			ServiceHost host;
+            var metadataBehavior = host.Description.Behaviors.Find<ServiceMetadataBehavior>();
 
-			if (serviceType.IsDataService())
-			{
-				host = new WcfDataServicesHost(serviceType, baseAddresses);
-			}
-			else
-			{
-				host = new ServiceLocatorServiceHost(serviceType, baseAddresses);
-			}
+            if (metadataBehavior == null)
+            {
+                metadataBehavior = new ServiceMetadataBehavior { HttpGetEnabled = true };
+                host.Description.Behaviors.Add(metadataBehavior);
+            }
+            else
+            {
+                metadataBehavior.HttpGetEnabled = true;
+            }
+        }
 
-			foreach (var iface in serviceType.GetInterfaces().Where(i => i != typeof(IRequestHandler)))
-			{
-				var attr = (ServiceContractAttribute)Attribute.GetCustomAttribute(iface, typeof(ServiceContractAttribute));
+        public override ServiceHostBase CreateServiceHost(string constructorString, Uri[] baseAddresses)
+        {
+            var serviceType = GetType(constructorString);
 
-				if (attr == null)
-				{
-					continue;
-				}
+            ServiceHost host;
 
-				ConfigureService(iface, host);
-				break;
-			}
+            if (serviceType.IsDataService())
+            {
+                host = new WcfDataServicesHost(serviceType, baseAddresses);
+            }
+            else
+            {
+                host = new ServiceLocatorServiceHost(serviceType, baseAddresses);
+            }
 
-			return host;
-		}
-	}
+            foreach (var iface in serviceType.GetInterfaces().Where(i => i != typeof(IRequestHandler)))
+            {
+                var attr = (ServiceContractAttribute)Attribute.GetCustomAttribute(iface, typeof(ServiceContractAttribute));
+
+                if (attr == null)
+                {
+                    continue;
+                }
+
+                ConfigureService(iface, host);
+                break;
+            }
+
+            return host;
+        }
+    }
 }
